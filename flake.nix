9aefd874dcd7d8adf8ebb512523f08370755bfb7
Hunter <hdh20267@uga.edu>
2024-03-28 11:03:31 -0400

    update backup location

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     kioskBase.url = "github:georgiaaim/nixos-kiosk-base"; # Adjust the URL/path to your flake
     kioskBase.inputs.nixpkgs.follows = "nixpkgs";
+    kioskBase.inputs.home-manager.follows = "home-manager";
   };
 
-  outputs = { self, nixpkgs, kioskBase, ... }: {
+  outputs = { self, nixpkgs, home-manager, kioskBase, ... }: {
     nixosConfigurations.IOTVignette = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -14,6 +16,7 @@
         ({ pkgs, lib, ... }: {
           # Any additional system-specific configuration
           networking.hostName = lib.mkForce "IOTVignette";
+          home-manager.users.kioskadmin.home.file."backup.tar".source = ./HA_Vignette_Backup.tar;
         })
       ];
     };
